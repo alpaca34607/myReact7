4ca9916d10551b354e7ce875c3bb7bba0bd99c9e
alpaca34607 <alpaca34607@gmail.com>
2024-11-06 13:58:27 +0800

    1106

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,19 +1,32 @@
-import MyComponent from "./component/MyComponent"
-function MyComponent2() {
+
+import "./App.css"
+/*  空標籤:Fragment */
+function MyComponent() {
   return(
-  // 空標籤:Fragment
+
   <>
-  <h1>我是元件2</h1>
+  <h2>我是元件2</h2>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ratione inventore minus nobis enim quaerat illum modi impedit ducimus tempora molestiae, harum cupiditate deserunt corporis, ipsam officia velit eaque dolore?</p>
   </>
   )
 }
 function App() {
 
-
+// 建立變數
+const strName="建仔";
   return (
     <div> 
-      <MyComponent2/>
+      {/* JSX中使用javascript變數，前後加大括號{變數名稱} */}
+      <h1 className="myH1" style={{
+        color:'red',
+        background:'yellow',
+        width:'300px',
+      }}>{strName.toUpperCase()}, 午安</h1>
+      {{/* 屬性中使用變數 */}}
+      <label htmlFor="userName">請輸入姓名:</label>
+      {/* {placeholdder => 提示字} */}
+      <input type="text" id="userName" placeholder={strName}/>
+     { {/* <MyComponent/> */}}
       </div>
   )
 }
